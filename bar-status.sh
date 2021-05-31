ff1dc3ba9f905c2d9fd877b825a1ce2e17399acb
1990frog <1990frog@gmail.com>
2021-05-31 21:36:43 +0000

    commit

--- bar-status.sh
+++ bar-status.sh
@@ -3,31 +3,26 @@
 # 声音
 get_volume() {
 	volume="$(amixer get PCM | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')"
-	if test "$volume" -gt 0
-	then
-		echo -e "音量：${volume}%"
-	else
-		echo -e "Mute"
-	fi
+	echo -e "${volume}%"
 }
 
 # CPU
 get_cpu(){
-	echo -e `top -b -n1 | grep ^%Cpu | awk '{printf("CPU：%.2f%"), 100-$8}'`
+	echo -e `top -b -n1 | grep ^%Cpu | awk '{printf("%.2f%"), 100-$8}'`
 }
 
 # 内存
 get_memory(){
-	echo -e `free --mebi | sed -n '2{p;q}' | awk '{printf ("内存：%2.2fGiB/%2.2fGiB(%.2f%)\n", ( $3 / 1024), ($2 / 1024), ($3/$2*100))}'`
+	echo -e `free --mebi | sed -n '2{p;q}' | awk '{printf ("%2.2fGiB/%2.2fGiB(%.2f%)\n", ( $3 / 1024), ($2 / 1024), ($3/$2*100))}'`
 }
 
 # 日期
 get_date(){
-	echo -e "时间：$(date +'%Y年%m月%d日 %H:%M')"
+	echo -e "$(date +'%Y年%m月%d日 %H:%M')"
 }
 
+# 轮询刷新
 while true; do
-	xsetroot -name "$(get_cpu) , $(get_memory) , $(get_date) , $(get_volume)"
+	xsetroot -name "cpu:$(get_cpu)，mem:$(get_memory)，time:$(get_date)，volume:$(get_volume)"
 	sleep 1s
 done
-# xsetroot -name "$(get_cpu) , $(get_memory) , $(get_date) , $(get_volume)"
